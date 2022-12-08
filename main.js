149275e526353fbe534022d75b371eb6db2b4b8b
THE ANH <20521081@gm.uit.edu.vn>
2022-12-08 16:52:33 +0700

    add all file firstime

--- main.js
+++ main.js
@@ -63,7 +63,7 @@ for(let i =0 ;i < cate_list.length;i++)
      }
 }
 /*Modal-JS*/
-var buttons=document.querySelectorAll('.menu_item');
+/*var buttons=document.querySelectorAll('.menu_item');
 var modal=document.querySelector('.modal_lotteria');
 var btn_close_1=document.querySelector('.btn_close_1');
 var btn_close_2=document.querySelector('.btn_close_2');
@@ -95,7 +95,7 @@ btn_next.addEventListener('click',function(){
 btn_back.addEventListener('click',function(){
   modal_login.classList.remove('flex');
   modal_login_back.classList.add('flex');
-  })
+  })*/
 /*Product*/
 let API1='http://localhost:3000/posts';
 let API2='http://localhost:3000/comments';
@@ -180,6 +180,143 @@ function GetItems2()
     })
     .then(RenderItem);
 }
+/*Account infomation*/
+let Btn_info_account=document.querySelector('.drop_menu');
+x1=document.querySelector('.block_infomation');
+x1.onclick=function(event)
+{
+  event.stopPropagation();
+}
+Btn_info_account.addEventListener('click',function(){
+  if(document.querySelector('.cart_content').classList.contains('none')==false)
+  {
+    document.querySelector('.cart_content').classList.add('none');
+  }
+  document.querySelector('.block_infomation').classList.toggle('none');
+})
+/*Cart*/
+let btn_cart=document.querySelector('.drop_dow_cart');
+x2=document.querySelector('.cart_content');
+x2.onclick=function(event)
+{
+  event.stopPropagation();
+}
+btn_cart.onclick=function()
+{
+  if(document.querySelector('.block_infomation').classList.contains('none')==false)
+  {
+    document.querySelector('.block_infomation').classList.add('none');
+  }
+   document.querySelector('.cart_content').classList.toggle('none');
+}
+/*Xóa sản phẩm*/
+
+let current_price;
+let current_quantity;
+let total_money_text;
+let total_update;
+let total_money=document.querySelector('.total_money');
+function Delete_Cart()
+{
+if(parseFloat(total_money.innerText) > 0)
+{
+let array_btn_delete_cart=document.querySelectorAll('.btn_delete_cart_item');
+for( var i=0 ;i < array_btn_delete_cart.length ; i++)
+{
+    array_btn_delete_cart[i].onclick=function(event)
+    {  
+         total_money_text=total_money.innerText;
+         console.log(total_money_text);
+         current_price=event.target.parentElement.querySelector('.current_price').innerText;
+         console.log(current_price);
+         current_quantity=event.target.parentElement.querySelector('.quantity').innerText;
+         console.log(current_quantity);
+         event.target.parentElement.remove();/*Delete Item*/
+         /*Update total*/
+
+         total_update=parseFloat(total_money_text)-parseFloat(current_price)*parseFloat(current_quantity);
+         total_money_text=total_update;
+         total_money.innerText=total_money_text + ".000đ";
+         console.log(total_money_text);
+    
+    }
+}
+}
+}
+
+/*---------Thêm sản phẩm---------- */
+let btn_add_to_cart=document.querySelectorAll('.btn_add-to-cart');/*Lấy ra danh sách các nút bấm */
+let image_cart_item;
+let name_cart_item;
+let price_cart_item;
+let cart_item_import;
+let cart_item_quantity;
+/*Lặp qua từng nút */
+for ( let i =0 ; i < btn_add_to_cart.length ; i ++)
+{
+   btn_add_to_cart[i].onclick=function(event)
+   {     let flag=false;
+         image_cart_item=event.target.parentElement.querySelector('img').src;
+         name_cart_item=event.target.parentElement.querySelector('h5').innerText;
+         price_cart_item=event.target.parentElement.querySelector('.price').innerText;
+         let field_namee=document.querySelectorAll('.field_name');
+         
+         /*Kiem tra san pham đã có trong cart hay chưa */
+          for( var i =0 ; i < field_namee.length ; i++)
+          {  /*nếu có rồi thì cập nhật lại số lượng */
+            if(name_cart_item === field_namee[i].innerText)
+            {    cart_item_quantity=field_namee[i].parentElement.querySelector('.quantity').innerText;
+                 quantity_temp=parseFloat(cart_item_quantity);
+                 quantity_temp+=1;
+                 flag=true;
+                 field_namee[i].parentElement.querySelector('.quantity').innerText=`${quantity_temp}`;
+                 
+                 break;
+            }
+          }
+          /*nếu chưa có thì thêm item mới vào cart */
+         if (flag == false)
+          {
+         cart_item_import=`
+         
+                             <button class="btn_delete_cart_item">
+                              
+                             </button>
+                             <div class="field_img_cart">
+                              <a href="">
+                                <img src="${image_cart_item}" alt="">
+                              </a>
+                             </div>
+                             <div class="field_info">
+                                   <p class="field_name ">${name_cart_item}</p>
+                                   <div class="field_price">
+                                    <span class="current_price">${price_cart_item}</span>
+                                    <span> x</span>
+                                    <span class="quantity">1</span>
+                                   </div>
+                             </div>
+                             
+                       
+         `
+         var node_cart_item=document.createElement('li');
+         node_cart_item.innerHTML=cart_item_import;
+         node_cart_item.classList.add('cart_item');
+         document.querySelector('.cart_list ul').appendChild(node_cart_item);
+          }
+          
+        /*Cập nhật lại tiền */
+          total_money_text=total_money.innerText;
+          
+          total_update=parseFloat(total_money_text)+parseFloat(price_cart_item)
+          total_money_text=total_update;
+          total_money.innerText=total_money_text + ".000đ";
+          Delete_Cart();
+   }
+}
+
+
+
+
 
 
 
